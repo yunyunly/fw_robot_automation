e95884b9b3dce3a2dc5b230c25b0b71e0af5482d
fanyx <fanyuxin@hiorka.com>
2023-10-27 07:37:28 +0100

    feat(serial) parallel match

--- lib/SerialLib.py
+++ lib/SerialLib.py
@@ -3,79 +3,99 @@ import datetime
 import time 
 import re 
 import threading 
+import asyncio
+from uuid import uuid4 
 
 class Logger(object):
     def __init__(self):
-        self._io_opened = False
-        self._opened = False 
-        self._wait_new_line = False
-        self._has_new_line = threading.Semaphore(0)
-        self._readed = 0
-        self._logs = []
-        self._idx = 0
+        self.io_opened = False
+        self.opened = False 
+        self.logs = []
+        self.total_idx = 0
+        self.clients = {}
+        self.client_history = 0
+        self.more = threading.Event()
         return 
-    
-    @staticmethod
-    def pr(msg):
-        print("[Logger]", msg)
 
-    def _io_handler(self):
-        self._io_opened = True 
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Logger, cls).__new__(cls)
+        return cls.instance
+
+    def io_handler(self):
+        self.io_opened = True 
         while (True) :
-            self.pr("check break...")
-            if self._opened == False:
+            print("check break...")
+            if self.opened == False:
                     break
             try:
-                Logger.pr("reading...")
+                print("reading...")
                 newline = self._serial.readline().decode("utf-8").strip()
                 if newline == "":
                     continue
-                Logger.pr(f"readline {newline}")
-                self.pr("check waiting...")
-                if self._wait_new_line == True:
-                    self.pr("notify wait flag...")
-                    self._wait_new_line = False
-                    self._has_new_line.release()
+                print(f"readline {newline}")
+                print("check waiting...")
+                if self.is_newline_waited():
+                    print("notify wait flag...")
+                    self.notify_newline()
             except:
                 newline = "serial closed"
             timestamp = datetime.datetime.now().strftime('%02H:%02M:%02S.%f')
-            log_entry = f"{self._idx}[{timestamp}] {newline}"
-            self._idx +=1
-            self._logs.append(log_entry)
-        self._io_opened = False 
+            log_entry = f"{self.total_idx}[{timestamp}] {newline}"
+            self.total_idx +=1
+            self.logs.append(log_entry)
+        self.io_opened = False 
 
     def open(self, port, bard_rate):
         self._serial = serial.Serial(port, bard_rate)
-        self._logs = []
-        self._logs.clear()
-        self._io = threading.Thread(target=self._io_handler)
-        self._opened = True 
-        self._io.start()
-        self._readed = 0
+        self.logs = []
+        self.logs.clear()
+        self.io = threading.Thread(target=self.io_handler)
+        self.opened = True 
+        self.more.clear()
+        self.io.start()
 
     def close(self):
-        self._opened = False
+        self.opened = False
         self._serial.close()
-        self._io.join()
+        self.io.join()
+    
+    def wait_newline(self):
+        self.more.clear()
+        self.more.wait()
+        return 
+    
+    def is_newline_waited(self):
+        return not self.more.is_set() 
 
-    def read_latest(self)->str|None:
-        if len(self._logs) == self._readed:
+    def notify_newline(self):
+        self.more.set()
+        return 
+
+    def read_latest(self, id)->str|None:
+        if id not in self.clients.keys():
+            self.clients[id] = self.client_history 
+        if len(self.logs) == self.clients[id]:
             return None 
-        newline = self._logs[self._readed]
-        self._readed += 1
+        newline = self.logs[self.clients[id]]
+        self.clients[id] += 1
+        if self.client_history < self.clients[id]:
+            self.client_history = self.clients[id]
         return newline
     
-    def read_latest_blocking(self)->str:
-        if len(self._logs) == self._readed:
-            self._wait_new_line = True 
-            self._has_new_line.acquire()
-        newline = self.read_latest() 
+    def read_latest_blocking(self, id)->str:
+        if id not in self.clients.keys():
+            self.clients[id] = self.client_history
+        if len(self.logs) == self.clients[id]:
+            self.wait_newline()
+        newline = self.logs[self.clients[id]]
+        self.clients[id] = self.clients[id] + 1
+        print(id, "read", newline)
         if newline == None:
             raise TypeError("Should not be None type")
+        if self.client_history < self.clients[id]:
+            self.client_history = self.clients[id]
         return newline
-
-    def read_newest(self)->str|None:
-        return self.read_latest()
     
     def write_string(self, data):
             data_bytes = data.encode()
@@ -93,10 +113,8 @@ class Logger(object):
             timestamp = datetime.datetime.now().strftime('%d:%H:%M:%S')
             filename = f"log-{timestamp}.txt"
             with open(filename, "a") as file:
-                for log_entry in self._logs:
+                for log_entry in self.logs:
                     file.write(log_entry+"\n")
-        
-
 
 
 class SerialLib(object):
@@ -107,7 +125,10 @@ class SerialLib(object):
     bard rate: 115200 
     """
     def __init__(self):
-        self._logger = Logger()
+        self.logger = Logger()
+        self.results = []
+        self.threads = []
+        self.threads_cnt = 0
         return 
 
     def open_serial_port(self, port, bard_rate):
@@ -117,13 +138,7 @@ class SerialLib(object):
         | Open Serial Port | "/dev/ttyUSB2" | 115200 |
         | Open Serial Port | "/dev/ttyUSB3" | 1152000 |
         """
-        self._logger.open(port, bard_rate)
-
-    def serial_save(self, filepath=None):
-        """Save current log into files.
-        The log file named log-{time}.txt and are stored in current working dir.
-        """
-        self._logger.save(filepath)
+        self.logger.open(port, bard_rate)
 
     def close_serial_port(self):
         """Close current serial port.
@@ -131,7 +146,13 @@ class SerialLib(object):
         Examples:
         | Close Serial Port|
         """
-        self._logger.close()
+        self.logger.close()
+
+    def serial_save(self, filepath=None):
+        """Save current log into files.
+        The log file named log-{time}.txt and are stored in current working dir.
+        """
+        self.logger.save(filepath)
     
     def serial_read_latest(self):
         """Read the latest line of serial port 
@@ -144,31 +165,18 @@ class SerialLib(object):
         Examples:
         |${data}=|Serial Read Latest|
         """
-        return self._logger.read_latest()
-    
-    def serial_read_newest(self):
-        """(deprecated)Read the newest line of serial port 
-        One line of serial data can be returned only once by this call 
-
-        Return:
-        None if no new serial data since last read 
-        String otherwise 
-
-        Examples:
-        |${data}=|Serial Read Newest|
-        """
-        return self._logger.read_newest()
+        return self.logger.read_latest(uuid4())
     
     def serial_read_blocking(self)->str:
-        return self._logger.read_latest_blocking()
-
+        return self.logger.read_latest_blocking(uuid4())
+    
     def serial_write_string(self, data):
         """Write string to serial port.
 
         Examples:
         | Serial Write String | Hello, World! |
         """
-        return self._logger.write_string(data)
+        return self.logger.write_string(data)
     
     def serial_write_hex(self, data):
         """Write hex data to serial port. 
@@ -176,9 +184,9 @@ class SerialLib(object):
         Examples:
         |Serial Write Hex| abcdeef |
         """
-        return self._logger.write_hex(data) 
+        return self.logger.write_hex(data) 
 
-    def serial_read_until(self, expected, timeout=None):
+    def serial_read_until(self, expected, timeout=None, parallel_idx = None):
         """Wait for a specific string from the serial port.
 
         This function will continuously listen for data until the expected string is received or the timeout is reached.
@@ -192,17 +200,26 @@ class SerialLib(object):
         | Serial Read Until | Hello, World! | timeout=10 |
         """
         start_time = time.time()
+        ret = ""
+        id =uuid4()
         while True:
-            newline = self.serial_read_blocking()
+            newline = self.logger.read_latest_blocking(id)
+            print("until check", newline)
             match = re.search(expected, newline)
 
             if match:
-                return match.string
+                ret = match.string 
+                break
             
             if timeout is not None and time.time() - start_time >= timeout:
-                return None 
+                ret = None 
+                break 
+        if parallel_idx == None :
+            return ret 
+        else:
+            self.results[parallel_idx] = ret
     
-    def serial_read_until_regex(self, data_pattern, timeout=None):
+    def serial_read_until_regex(self, data_pattern, timeout=None, parallel_idx = None):
         """Wait for string matching a given regular expression pattern.
 
         This function will continuously listen for data until data matching the specified pattern is received or the timeout is reached.
@@ -220,34 +237,73 @@ class SerialLib(object):
         | ${datas} = | Serial Read Until Regex | Data: (\w+) (\d+) | timeout=10 |
         """
         start_time = time.time()
+        ret = None
+        id = uuid4()
         while True:
-            newline = self.serial_read_blocking()
-            print("regex get nl", newline)
+            newline = self.logger.read_latest_blocking(id)
             match = re.search(data_pattern, newline)
 
             if match:
-                print(match.groups)
-                return match.groups()
+                ret = match.groups()
+                break
             
             if timeout is not None and time.time() - start_time >= timeout:
-                return None 
+                ret = None 
+                break
+        if parallel_idx == None :
+            return ret 
+        else:
+            self.results[parallel_idx] = ret
+        
+    def serial_parallel_read_until(self, expected, timeout=None):
+        self.results.append(None)
+        self.threads.append(threading.Thread(target = self.serial_read_until, args = (expected, timeout, self.threads_cnt)))
+        self.threads_cnt += 1
+    
+    def serial_parallel_read_until_regex(self, data_pattern, timeout = None): 
+        self.results.append(None)
+        self.threads.append(threading.Thread(target = self.serial_read_until_regex, args = (data_pattern, timeout, self.threads_cnt)))
+        self.threads_cnt += 1
+
+    def serial_parallel_wait(self):
+        for i in self.threads:
+            i.start()
+        for i in self.threads:
+            i.join()
+        ret = self.results.copy()
+        self.threads_cnt = 0
+        self.threads.clear()
+        self.results.clear()
+        return ret
+
 
 if __name__ == "__main__":
     sl = SerialLib()
     sl.open_serial_port("/dev/ttyACM0", 115200)
-    for i in range(1, 40):
-        print("newest",sl.serial_read_newest())
-        time.sleep(0.1)
-    
-    for i in range(1, 5):
-        print("latest", sl.serial_read_latest())
-
-    for i in range(1, 5):
-        print("blocking", sl.serial_read_blocking())
-    for i in range(1, 5):
-        print("string", sl.serial_read_until("heartbeat"))
-
-    for i in range (1, 5):
-        print("int", sl.serial_read_until_regex("shutdown count down ([0-9]+)"))
-    sl.save()
+    # for i in range(1, 40):
+    #     print("newest",sl.serial_read_newest())
+    #     time.sleep(0.1)
+    # 
+    # for i in range(1, 5):
+    #     print("latest", sl.serial_read_latest())
+    #
+    # for i in range(1, 5):
+    #     print("blocking", sl.serial_read_blocking())
+    # for i in range(1, 5):
+    #     print("string", sl.serial_read_until("heartbeat"))
+    #
+    # for i in range (1, 5):
+    #     print("int", sl.serial_read_until_regex("shutdown count down ([0-9]+)"))
+    # sl.save()
+    sl.serial_parallel_read_until_regex("Btn: ([a-zA-Z]+)")
+    # print(a)
+    sl.serial_parallel_read_until("Btn: wait")
+    # print(b)
+    results = sl.serial_parallel_wait()
+    print(results)
+    sl.serial_parallel_read_until("Btn: notified")
+    sl.serial_parallel_read_until("Btn: wait")
+    results = sl.serial_parallel_wait()
+    print(results)
+    # # results = await asyncio.gather(*tasks)
     sl.close_serial_port()
