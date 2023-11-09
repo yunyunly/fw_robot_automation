282ed84d589d5d43077f9dc8d29e8bc0452d0ad5
fanyx-ok <fanyuxin@hiorka.com>
2023-11-09 10:06:52 +0000

    feat(core) basic core test cases

--- lib/BluetoothLib.py
+++ lib/BluetoothLib.py
@@ -37,16 +37,19 @@ class BluetoothLib(object):
         dev_addr = addr.replace(":", "_")
         dev_path = f'/org/bluez/hci{self.hci_id}/dev_{dev_addr}'
         Console(f"Connect To {dev_path}")
-        Console("Discovering Device")
+        Console("scan device")
         cached = len([x for x in self.om_if.GetManagedObjects() if dev_addr in x]) != 0
         while not cached:
-            self.adapter_obj.StartDiscovery(dbus_interface="org.bluez.Adapter1")
-            while not cached:
-                print("Press Reset Let Device Discovable!!!")
-                time.sleep(1)
-                print("founded")
-                cached = True 
-            self.adapter_obj.StopDiscovery(dbus_interface="org.bluez.Adapter1")
+            print(".", end="")
+            time.sleep(0.4)
+            cached = len([x for x in self.om_if.GetManagedObjects() if dev_addr in x]) != 0
+            # self.adapter_obj.StartDiscovery(dbus_interface="org.bluez.Adapter1")
+            # while not cached:
+            #     print("Press Reset Let Device Discovable!!!")
+            #     time.sleep(1)
+            #     print("founded")
+            #     cached = True 
+            # self.adapter_obj.StopDiscovery(dbus_interface="org.bluez.Adapter1")
         Console("Get Connection State")
         dev_obj = self.bus.get_object("org.bluez", dev_path)
         dev_prop = dbus.Interface(dev_obj, "org.freedesktop.DBus.Properties")
@@ -69,7 +72,7 @@ class BluetoothLib(object):
         print("Connected: ", dev_connected)
 
         return (dev_obj, dev_path)
-
+    
     def send(self, iface, data):
         """Send data to connected device via BLE 
         
@@ -83,41 +86,78 @@ class BluetoothLib(object):
         You should not use it in test cases 
         """
         return iface.ReadValue("")
- 
+    
+    def get_gatt_char(self, dev_path, srv_id, char_id):
+        char_path = f"{dev_path}/{srv_id}/{char_id}"
+        cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        print("scan char")
+        while not cached:
+            print(".", end="")
+            time.sleep(1)
+            cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        return dbus.Interface(self.bus.get_object("org.bluez", char_path), "org.bluez.GattCharacteristic1")
+
+    def ble_scan_on(self):
+        try:
+            self.adapter_obj.StartDiscovery(dbus_interface="org.bluez.Adapter1")
+        except:
+            raise Exception("scan on failed")
+    def ble_scan_off(self):
+        try:
+            self.adapter_obj.StopDiscovery(dbus_interface="org.bluez.Adapter1")
+        except:
+            raise Exception("scan off failed")
+
     def ble_connect_case(self, addr = "D0:14:11:20:20:18"):
         """Connect to a given address charging case, no matter whether it is cached"""
         print("Blue Connect Case...")
+        self.ble_scan_on()
         self.case, self.case_path = self.connect(addr)
-        char_path = f'{self.case_path}/service0019/char001a'
-        print("Blue Discovering Char...")
-        cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
-        while not cached:
-            print("Discovering service and characteristic...")
-            time.sleep(1)
-            cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
-        self.case_char_if = dbus.Interface(self.bus.get_object("org.bluez", char_path), "org.bluez.GattCharacteristic1")
+        self.case_char_if = self.get_gatt_char(self.case_path, "service0019", "char001a")
+        # char_path = f'{self.case_path}/service0019/char001a'
+        # print("Blue Discovering Char...")
+        # cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        # while not cached:
+        #     print("Discovering service and characteristic...")
+        #     time.sleep(1)
+        #     cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        # self.case_char_if = dbus.Interface(self.bus.get_object("org.bluez", char_path), "org.bluez.GattCharacteristic1")
         print("Case Char Interface", self.case_char_if)
         self.case_addr = addr
+        self.ble_scan_off()
         return 
     
     def ble_connect_hearing_aids(self, addr = "D0:14:11:20:20:18"):
         """Connect to given address hearing aids, no matter whether it is cached"""
         print("Blue Connect Hearing Aids...")
+        self.ble_scan_on()
         self.hearing_aids, self.hearing_aids_path = self.connect(addr)
-        char_path = f'{self.hearing_aids_path}/service001a/char001f' 
-        cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
-        while not cached:
-            print("Discovery service and characteristic")
-            time.sleep(1)
-            cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
-        self.hearing_aids_char_if = dbus.Interface(self.bus.get_object("org.bluez", char_path), "org.bluez.GattCharacteristic1")
+        self.hearing_aids_char_if = self.get_gatt_char(self.hearing_aids_path, "service001a", "char001f")
+        # char_path = f'{self.hearing_aids_path}/service001a/char001f' 
+        # cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        # while not cached:
+        #     print("Discovery service and characteristic")
+        #     time.sleep(1)
+        #     cached = len([x for x in self.om_if.GetManagedObjects() if char_path in x]) != 0
+        # self.hearing_aids_char_if = dbus.Interface(self.bus.get_object("org.bluez", char_path), "org.bluez.GattCharacteristic1")
         print("HA Char Interface", self.hearing_aids_char_if)
         self.hearing_aids_addr = addr
+        self.ble_scan_off()
         return 
     
-    def ble_disconnect_case(self):
+    def ble_disconnect_case(self, dev_addr=None):
         """Not implemented yet"""
-        return 
+        if dev_addr is None:
+            dev_addr = self.case_addr
+        device_path = f"/org/bluez/hci{self.hci_id}/dev_" + dev_addr.replace(":", "_")
+        device = dbus.Interface(self.bus.get_object("org.bluez", device_path),"org.bluez.Device1")
+        ret = False 
+        try:
+            device.Disconnect()
+            ret = True 
+        except dbus.DBusException as e:
+            print(e)
+        return ret  
     
     def ble_disconnect_hearing_aids(self, dev_addr=None):
         if dev_addr is None:
@@ -138,10 +178,20 @@ class BluetoothLib(object):
         return 
     
     def ble_case_connected(self, dev_addr=None):
-        """Not implemented yet"""
-        return 
+        """Return True if charging case connected, otherwise False"""
+        if dev_addr is None:
+            dev_addr = self.case_addr
+        dev_path = f"/org/bluez/hci{self.hci_id}/dev_" + dev_addr.replace(":", "_")
+        try:
+            device = dbus.Interface(self.bus.get_object("org.bluez", dev_path), "org.freedesktop.DBus.Properties")
+            ret = device.Get("org.bluez.Device1", "Connected")
+            return ret == dbus.Boolean(1)
+        except dbus.exceptions as e: 
+            print(e)
+            return False
 
     def ble_hearing_aids_connected(self, dev_addr=None):
+        """Return True if hearing aids connected, otherwise False"""
         if dev_addr is None:
             dev_addr = self.hearing_aids_addr
         dev_path = f"/org/bluez/hci{self.hci_id}/dev_" + dev_addr.replace(":", "_")
@@ -149,20 +199,60 @@ class BluetoothLib(object):
             device = dbus.Interface(self.bus.get_object("org.bluez", dev_path), "org.freedesktop.DBus.Properties")
             ret = device.Get("org.bluez.Device1", "Connected")
             return ret == dbus.Boolean(1)
-        except:
+        except dbus.exceptions as e: 
+            print(e)
             return False
+    
+    def ble_pair(self, dev_addr=None):
+        """Not work well"""
+        if dev_addr is None:
+            dev_addr = self.hearing_aids_addr 
+        dev_path = f"/org/bluez/hci{self.hci_id}/dev_" + dev_addr.replace(":", "_")
+        print("dev path", dev_path)
+        self.ble_scan_on()
+        cached = len([x for x in self.om_if.GetManagedObjects() if dev_path in x]) != 0
+        print("scan device")
+        while not cached:
+            print(".", end="")
+            time.sleep(0.4)
+            cached = len([x for x in self.om_if.GetManagedObjects() if dev_path in x]) != 0 
+        ret = False 
+        try:
+            iface = dbus.Interface(self.bus.get_object("org.bluez", dev_path), "org.bluez.Device1")
+            print(iface, type(iface))
+            ret = iface.Pair({}) 
+            print(ret ,type(ret))
+        except dbus.DBusException as e:
+            print(e)
+        self.ble_scan_off()
+        return ret 
 
+    def ble_unpair(self, dev_addr=None):
+        """Not work well"""
+        if dev_addr is None:
+            dev_path = self.hearing_aids_addr
+        else:
+            dev_path = f"/org/bluez/hci{self.hci_id}/dev_{dev_addr}".replace(":","_")
+        print("unpair", dev_path)
+        hci_path = f"/org/bluez/hci{self.hci_id}"
+        try:
+            iface = dbus.Interface(self.bus.get_object("org.bluez", hci_path), "org.bluez.Adapter1")
+            ret = iface.RemoveDevice(dev_path)
+            return ret 
+        except dbus.DBusException as e:
+            print(e)
+            return False
       
-if __name__ == "__main__":
-    ble = BluetoothLib()
-    ble.ble_connect_hearing_aids("12:34:56:78:A0:B3")
-    ble.ble_connect_case()
-    header = [9]
-    cmd = [0,11]
-    desc = [0,0]
-    param = [1] 
-    ble.ble_hearing_aids_connected("12:34:56:78:A0:B3")
-
-    ble.ble_disconnect_hearing_aids()
-    ble.ble_disconnect_case()
+# if __name__ == "__main__":
+#     ble = BluetoothLib()
+#     ble.ble_connect_hearing_aids("12:34:56:78:A0:B3")
+#     ble.ble_connect_case()
+#     header = [9]
+#     cmd = [0,11]
+#     desc = [0,0]
+#     param = [1] 
+#     ble.ble_hearing_aids_connected("12:34:56:78:A0:B3")
+#
+#     ble.ble_disconnect_hearing_aids()
+#     ble.ble_disconnect_case()
 
--- serial_view.py
+++
@@ -1,5 +0,0 @@
-import serial 
-
-s = serial.Serial("/dev/ttyACM0", 115200)
-while True:
-    print(s.readline().decode("utf-8").strip())      
